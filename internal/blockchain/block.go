59b530e074fe36c1941402e2823f6361f51c5b96
milad <miladbachari14@gmail.com>
2024-10-25 16:24:08 +0330

    feat: add add block

--- internal/blockchain/block.go
+++ internal/blockchain/block.go
@@ -1,37 +1,25 @@
 package blockchain
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
+
+	"github.com/sg-milad/stupid-blockchain/internal/types"
+	"github.com/sg-milad/stupid-blockchain/pkg/pow"
 )
 
-// Block keeps block headers
-type Block struct {
-	Timestamp     int64
-	Data          []byte
-	PrevBlockHash []byte
-	Hash          []byte
-}
-
-// SetHash calculates and sets block hash
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
-}
-
 // NewBlock creates and returns Block
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+func NewBlock(data string, prevBlockHash []byte) *types.Block {
+	block := &types.Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := pow.NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
 // NewGenesisBlock creates and returns genesis Block
-func NewGenesisBlock() *Block {
+func NewGenesisBlock() *types.Block {
 	return NewBlock("Genesis Block", []byte{})
 }
