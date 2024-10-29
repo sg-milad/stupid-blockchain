33b2469c2d7961a2506584366ceecf20f6fb70ca
milad <miladbachari14@gmail.com>
2024-10-29 12:56:32 +0330

    refactor: main

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	cli "github.com/sg-milad/stupid-blockchain/cmd/blockchain"
-	"github.com/sg-milad/stupid-blockchain/internal/blockchain"
 )
 
 func main() {
-	bc := blockchain.NewBlockchain()
-	defer bc.Db.Close()
-
-	cli := cli.CLI{bc}
+	cli := cli.CLI{}
 	cli.Run()
 }
